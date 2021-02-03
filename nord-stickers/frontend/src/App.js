64cfd10cfbad235b38d085b1bd9798bce18060f7
AmjadLille <sammanamjad86@gmail.com>
2021-02-03 11:32:08 +0100

    preaperd front

---
+++ nord-stickers/frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      HIIIIII
+      <Footer />
+    </>
+  );
+};
+
+export default App;
